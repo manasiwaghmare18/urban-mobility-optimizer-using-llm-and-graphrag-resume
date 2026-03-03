3ab471df9df7cf671ff6b0cb979a5e608d4d5843
Mansi Waghmare <manasiwaghmare18@gmail.com>
2026-03-03 13:01:39 +0530

    Update graph_schema.py

--- graph_db/graph_schema.py
+++ graph_db/graph_schema.py
@@ -6,6 +6,7 @@ def create_constraints(conn):
         "CREATE CONSTRAINT IF NOT EXISTS FOR (r:RoadSegment) REQUIRE r.id IS UNIQUE",
         "CREATE CONSTRAINT IF NOT EXISTS FOR (b:BusStop) REQUIRE b.id IS UNIQUE",
         "CREATE CONSTRAINT IF NOT EXISTS FOR (m:MetroStation) REQUIRE m.id IS UNIQUE"
+        "CREATE CONSTRAINT IF NOT EXISTS FOR (w:Weather) REQUIRE w.location_id IS UNIQUE"
     ]
 
     for q in queries:
