c67f1beea1ff2140d6b4196791e94aa820639d8f
Mansi Waghmare <manasiwaghmare18@gmail.com>
2026-03-03 11:54:14 +0530

    Added config folder

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+# config/settings.py
+import os
+
+NEO4J_URI="neo4j+s://afce61b2.databases.neo4j.io"
+NEO4J_USERNAME="afce61b2"
+NEO4J_PASSWORD="K37XzAXzHShU0j0S_IM_sINk-9KEhEINz357B4JKQUo"
+NEO4J_DATABASE="afce61b2"
+
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
