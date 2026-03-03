1175945fe1dd87d8d7186d29bc1aaf7b496a6383
Mansi Waghmare <manasiwaghmare18@gmail.com>
2026-03-03 14:05:09 +0530

    Fixed Neo4j 5 constraint syntax

--- graph_db/graph_schema.py
+++ graph_db/graph_schema.py
@@ -1,15 +1,37 @@
-# graph_db/graph_schema.py
-
 def create_constraints(conn):
+
     queries = [
-        "CREATE CONSTRAINT IF NOT EXISTS FOR (l:Location) REQUIRE l.id IS UNIQUE",
-        "CREATE CONSTRAINT IF NOT EXISTS FOR (r:RoadSegment) REQUIRE r.id IS UNIQUE",
-        "CREATE CONSTRAINT IF NOT EXISTS FOR (b:BusStop) REQUIRE b.id IS UNIQUE",
-        "CREATE CONSTRAINT IF NOT EXISTS FOR (m:MetroStation) REQUIRE m.id IS UNIQUE"
-        "CREATE CONSTRAINT IF NOT EXISTS FOR (w:Weather) REQUIRE w.location_id IS UNIQUE"
+
+        """
+        CREATE CONSTRAINT IF NOT EXISTS
+        FOR (l:Location)
+        REQUIRE l.id IS UNIQUE
+        """,
+
+        """
+        CREATE CONSTRAINT IF NOT EXISTS
+        FOR (b:BusStop)
+        REQUIRE b.id IS UNIQUE
+        """,
+
+        """
+        CREATE CONSTRAINT IF NOT EXISTS
+        FOR (m:MetroStation)
+        REQUIRE m.id IS UNIQUE
+        """,
+
+        """
+        CREATE CONSTRAINT IF NOT EXISTS
+        FOR (r:RoadSegment)
+        REQUIRE r.id IS UNIQUE
+        """,
+
+        """
+        CREATE CONSTRAINT IF NOT EXISTS
+        FOR (w:Weather)
+        REQUIRE w.location_id IS UNIQUE
+        """
     ]
 
     for q in queries:
         conn.execute(q)
-
-    print("Graph constraints created.")
