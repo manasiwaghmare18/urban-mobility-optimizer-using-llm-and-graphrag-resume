fdf3dab92697c126fd659ecacb3874c2d8a740f6
Mansi Waghmare <manasiwaghmare18@gmail.com>
2026-03-05 13:24:21 +0530

    Update settings.py
    Refactor Neo4j configuration to use environment variables.

--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,12 @@
-# config/settings.py
+
 import os
+from dotenv import load_dotenv
 
-NEO4J_URI="neo4j+s://afce61b2.databases.neo4j.io"
-NEO4J_USERNAME="afce61b2"
-NEO4J_PASSWORD="K37XzAXzHShU0j0S_IM_sINk-9KEhEINz357B4JKQUo"
-NEO4J_DATABASE="afce61b2"
+load_dotenv()
 
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+NEO4J_DATABASE = os.getenv("NEO4J_DATABASE")
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
